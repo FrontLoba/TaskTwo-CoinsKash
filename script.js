7e4c640e4ed264c7a436adf3e5bdd113569b0336
FrontLoba <lobaglory@gmail.com>
2022-08-29 06:51:44 +0100

    Initializing the java script for password visibility and international telephone input number

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
 // JavaScript for the Mobile Menu List
-
+/*
 const openBtn = document.getElementById('open-menu');
 const closeBtn = document.getElementById('close-menu');
 const menuList = document.getElementById('mobile-btn');
-
-
+const navbar = document.querySelector('.mobile-menu')
 
 openBtn.addEventListener('click', () => {
-
-    menuList.classList.add('mobile-menu');
-        // menuList.style.display = 'block';
-        // closeBtn.style.display = 'block';
-        // openBtn.style.display = 'none';
+    menuList.style.display = 'block';
+    openBtn.style.display = 'none';
+    closeBtn.style.display = 'block';
+    openBtn.classList.remove('close-icon')
+    // closeBtn.classList.toggle('close-icon')
+  
   })
-
-closeBtn.addEventListener('click', () => {
-        // menuList.style.display = 'none';
-        // closeBtn.style.display = 'none';
-        // openBtn.style.display = 'block';
-        // menuList.className.add('open-icon')
-})
-
-
-// JavaScript for Caurosel 
-// const scrollSlide = document.querySelector('testimony');
-//     slideIndex = document.querySelector('t-holder');
-//     slideDirect = document.querySelector('h-scroll');
-// const i,
+  closeBtn.addEventListener('click', () => {
+    menuList.style.display = 'none';
+    openBtn.style.display = 'block';
+    closeBtn.style.display = 'none'
+    // openBtn.classList.toggle('open-icon')
+    closeBtn.classList.add('open-icon')
+  })*/
 
 
+ /*
+  const togglePassword = document.querySelector("#togglePswd");
+  const password = document.querySelector("#password");
+  
+  togglePassword.addEventListener("click", function () {
+      // toggle the type attribute
+      const type = password.getAttribute("type") === "password" ? "text" : "password";
+      password.setAttribute("type", type);
+      
+      // toggle the icon
+      type.classList.toggle("visible");
+  });*/
 
 
-// openBtn.addEventListener('click', () => {
-//         menuList.style.display = 'block';
-//         closeBtn.style.display = 'block';
-//         openBtn.style.display = 'none';
-//     } );
-//     openBtn.className.add('close-icon');
-// }
 
-// openMenu();
-
-// closeBtn.addEventListener('click', () => {
-//     menuList.classList.add('mobile-menu');
-//     // openBtn.style.display = 'block'
-// })
+  
